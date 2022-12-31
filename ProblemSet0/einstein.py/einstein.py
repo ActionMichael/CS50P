23dae2197eb92ab5445e9bd71f1af8d7b5e9e386
Huang Liang-Zheng <abcdefghijkinm37@gmail.com>
2022-12-31 16:35:37 +0800

    Create einstein.py

---
+++ ProblemSet0/einstein.py/einstein.py
@@ -0,0 +1,7 @@
+number = int(input("m: "))
+#print("E:" + number * 90000000000000000)
+"""Traceback (most recent call last):
+  File "einstein.py", line 2, in <module>
+    print("E:" + number * 90000000000000000)
+TypeError: must be str, not int"""
+print("E:", number * 90000000000000000)
