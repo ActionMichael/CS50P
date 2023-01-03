229fdc9ca6523e198d448b6ac9c4060d33d56596
Huang Liang-Zheng <abcdefghijkinm37@gmail.com>
2023-01-04 06:31:55 +0800

    Update test_project.py

--- project/test_project.py
+++ project/test_project.py
@@ -10,19 +10,19 @@ def test_crawler():
     # Call crawler function
     crawler('0050.tw')
     # Check if txt file exists in directory
-    assert os.path.exists('C:/Users/User/Desktop/CS50P/project/0050.tw-items.txt') == True
+    assert os.path.exists('THE PATH YOU ALREADY SAVE/0050.tw-items.txt') == True
 
 def test_regular():
     # Call regular function
     regular('0050.tw')
     # Check if txt file exists in directory
-    assert os.path.exists('C:/Users/User/Desktop/CS50P/project/0050.tw.csv') == True
+    assert os.path.exists('CTHE PATH YOU ALREADY SAVE/0050.tw.csv') == True
 
 def test_drawing_and_save():
     # Call drawing_and_save function
     drawing_and_save('0050.tw')
     # Check if png file exists in directory
-    assert os.path.exists('C:/Users/User/Desktop/CS50P/project/Stock Index of 0050.tw.png') == True
+    assert os.path.exists('THE PATH YOU ALREADY SAVE/Stock Index of 0050.tw.png') == True
 
 if __name__ == "__main__":
     main()
